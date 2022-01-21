0e5f82476e5793951b30340662e348ba54a3512f
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-21 14:48:57 +0000

    Deleted redundant methods from ScrambledDiary

--- ex4/lib/scrambleddiary.rb
+++ ex4/lib/scrambleddiary.rb
@@ -7,14 +7,6 @@ class ScrambledDiary
     @contents
   end
 
-  # def unscramble_by_advancing_chars(steps)
-  #   scrambled_chars = @contents.chars
-  #   plain_chars = scrambled_chars.map do |char|
-  #     (char.ord - steps).chr
-  #   end
-  #   @contents = plain_chars.join
-  # end
-
   def scramble(method)
     new_content = method.scramble(@contents)
     @contents = new_content
